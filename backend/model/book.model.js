47ae9d1e819018df42eaa5df5fa43cae85f2cca1
Aman Singh <amansingh1905245@gmail.com>
2024-09-12 23:36:10 +0530

    minor fixes

--- backend/model/book.model.js
+++ backend/model/book.model.js
@@ -4,7 +4,10 @@ const bookSchema = mongoose.Schema({
     name: String,
     price: Number,
     category: String,
-    image: String,
+    image: {
+        type: String,
+        default: "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1726099200&semt=ais_hybrid"
+    },
     title: String,
 });
 const Book = mongoose.model("Book", bookSchema);
--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -4,9 +4,9 @@ function Cards({ item }) {
   return (
     <>
       <div className="mt-4 my-3 p-3">
-        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
+        <div className="card w-92 h-[350px] bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border ">
           <figure>
-            <img src={item.image} alt="Shoes" />
+            <img src={item.image} alt="Book" className="h-full w-full object-contain"/>
           </figure>
           <div className="card-body">
             <h2 className="card-title">
