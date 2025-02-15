ace536493cf5dd6b9fd61096a5f41804599dd29f
Aman Singh <amansingh1905245@gmail.com>
2025-02-16 01:38:56 +0530

    add new files

---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("https://comicsverse.onrender.com/contact", {
+                name,
+                email,
+                message,
+            });
+            setSuccess(true);
+            setName("");
+            setEmail("");
+            setMessage("");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+        <Navbar/>
+            <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+                <div className="mt-16 items-center justify-center text-center">
+                    <h1 className="text-2xl  md:text-4xl ">
+                        Get in Touch <span className="text-green-500">:)!</span>
+                    </h1>
+                    <p className="mt-8">
+                        Have a question, comment, or just want to say hi? We'd
+                        love to hear from you!
+                    </p>
+                    <Link to="/">
+                        <button className="mt-3 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+                            Back
+                        </button>
+                    </Link>
+                </div>
+                <div className="mt-12">
+                    <form action="https://getform.io/f/amdpnrjb" method="POST">
+                        <label>
+                            Name:
+                            <input
+                                placeholder="Enter your name"
+                                type="text"
+                                name="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                className="w-full p-2 pl-10 text-sm text-gray-700"
+                            />
+                        </label>
+                        <label>
+                            Email:
+                            <input
+                                placeholder="Enter your email address"
+                                type="email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="w-full p-2 pl-10 text-sm text-gray-700"
+                            />
+                        </label>
+                        <label>
+                            Message:
+                            <textarea
+                            placeholder="Enter your message..."
+                                value={message}
+                                name="message"
+                                onChange={(e) => setMessage(e.target.value)}
+                                className="w-full p-2 pl-10 text-sm text-gray-700"
+                            />
+                        </label>
+                        <button
+                            type="submit"
+                            name="button"
+                            className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300"
+                        >
+                            Send
+                        </button>
+                        {success && (
+                            <p className="text-green-500">
+                                Message sent successfully!
+                            </p>
+                        )}
+                    </form>
+                </div>
+            </div>
+            <Footer/>
+        </>
+    );
+}
+
+export default Contact;
---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+function Signup() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || "/";
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        const userInfo = {
+            fullname: data.fullname,
+            email: data.email,
+            password: data.password,
+        };
+
+        try {
+            const res = await fetch(
+                "https://comicsverse.onrender.com/user/signup",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(userInfo),
+                }
+            );
+
+            const data = await res.json();
+            console.log(data);
+            if (data) {
+                toast.success("Signup Successfully");
+                navigate(from, { replace: true });
+            }
+            localStorage.setItem("Users", JSON.stringify(data.user));
+        } catch (err) {
+            console.log(err.message);
+            toast.error("Error: " + err.message);
+        }
+    };
+    return (
+        <>
+            <div className="flex h-screen items-center justify-center">
+                <div className=" w-[600px] ">
+                    <div className="modal-box">
+                        <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <Link
+                                to="/"
+                                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                            >
+                                ✕
+                            </Link>
+
+                            <h3 className="font-bold text-lg">Signup</h3>
+                            <div className="mt-4 space-y-2">
+                                <span>Name</span>
+                                <br />
+                                <input
+                                    type="text"
+                                    placeholder="Enter your fullname"
+                                    className="w-80 px-3 py-1 border rounded-md outline-none"
+                                    {...register("fullname", {
+                                        required: true,
+                                    })}
+                                />
+                                <br />
+                                {errors.fullname && (
+                                    <span className="text-sm text-red-500">
+                                        This field is required
+                                    </span>
+                                )}
+                            </div>
+                            {/* Email */}
+                            <div className="mt-4 space-y-2">
+                                <span>Email</span>
+                                <br />
+                                <input
+                                    type="email"
+                                    placeholder="Enter your email"
+                                    className="w-80 px-3 py-1 border rounded-md outline-none"
+                                    {...register("email", { required: true })}
+                                />
+                                <br />
+                                {errors.email && (
+                                    <span className="text-sm text-red-500">
+                                        This field is required
+                                    </span>
+                                )}
+                            </div>
+                            {/* Password */}
+                            <div className="mt-4 space-y-2">
+                                <span>Password</span>
+                                <br />
+                                <input
+                                    type="text"
+                                    placeholder="Enter your password"
+                                    className="w-80 px-3 py-1 border rounded-md outline-none"
+                                    {...register("password", {
+                                        required: true,
+                                    })}
+                                />
+                                <br />
+                                {errors.password && (
+                                    <span className="text-sm text-red-500">
+                                        This field is required
+                                    </span>
+                                )}
+                            </div>
+                            {/* Button */}
+                            <div className="flex justify-around mt-4">
+                                <button className="bg-green-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                                    Signup
+                                </button>
+
+                                <p className="text-xl">
+                                    Have account? Go on Login...
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Signup;
